8d140619bbae1a8d75ce9a5e9bccb14f0ca05086
Arief G <ariefgraifhan@gmail.com>
2021-12-01 20:18:31 +0700

    first Commit

---
+++ src/test/java/com/feirasoft/bnibootcamp/BniBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feirasoft.bnibootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BniBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
