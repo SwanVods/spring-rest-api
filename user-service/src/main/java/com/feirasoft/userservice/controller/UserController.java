66912d6f5a159fd2e10d87bb7d3cf2a0ca7c687b
Arief G <ariefgraifhan@gmail.com>
2021-12-30 18:11:40 +0700

    optimize repo

--- user-service/src/main/java/com/feirasoft/userservice/controller/UserController.java
+++
@@ -1,9 +0,0 @@
-package com.feirasoft.userservice.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/users")
-public class UserController {
-}
--- user-service/src/main/java/com/feirasoft/userservice/service/AuthService.java
+++
@@ -1,4 +0,0 @@
-package com.feirasoft.userservice.service;
-
-public interface AuthService {
-}
