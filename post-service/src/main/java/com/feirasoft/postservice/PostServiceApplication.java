43ecabdd584f94c49ba658834347919a9bdd372e
Arief G <ariefgraifhan@gmail.com>
2022-01-04 02:21:13 +0700

    update

--- post-service/src/main/java/com/feirasoft/postservice/PostServiceApplication.java
+++ post-service/src/main/java/com/feirasoft/postservice/PostServiceApplication.java
@@ -2,12 +2,8 @@ package com.feirasoft.postservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//@EnableSwagger2
 @SpringBootApplication
-//@EnableEurekaClient
 public class PostServiceApplication {
 
     public static void main(String[] args) {
