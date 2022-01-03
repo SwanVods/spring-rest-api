fde465dbe3e5cf785ea32af32129536fb51171e9
Arief G <ariefgraifhan@gmail.com>
2022-01-04 01:57:21 +0700

    replacing user-service with auth-service

--- post-service/src/main/java/com/feirasoft/postservice/PostServiceApplication.java
+++ post-service/src/main/java/com/feirasoft/postservice/PostServiceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//@EnableSwagger2
 @SpringBootApplication
 @EnableMongoRepositories(basePackageClasses = PostRepository.class)
 @EnableEurekaClient
--- user-service/src/main/java/com/feirasoft/userservice/model/user/UserRoles.java
+++
@@ -1,5 +0,0 @@
-package com.feirasoft.userservice.model.user;
-
-public enum UserRoles {
-    ADMIN, TRAINER, STUDENT
-}
